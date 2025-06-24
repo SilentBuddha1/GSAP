6d1477550677894208e688e7e050103f31b5e0ae
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-06-24 14:34:04 +0545

    3_BOX_ROTATING

---
+++ Basic/2/script.js
@@ -0,0 +1,26 @@
+gsap.to(".box1", {
+  x: 1000,
+  rotate: 360,
+  background: "blue",
+  duration: 3,
+  delay: 1,
+  scale: 0.5,
+});
+
+gsap.from(".box2", {
+  x: 1000,
+  rotate: 360,
+  background: "blue",
+  duration: 3,
+  delay: 1,
+  scale: 0.5
+});
+
+gsap.to(".box3", {
+  x: 1000,
+  rotate: 360,
+  background: "red",
+  duration: 3,
+  delay: 1,
+  scale: 0.5,
+});
