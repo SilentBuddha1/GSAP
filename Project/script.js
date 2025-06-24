c527703de8e5c9a9b6b78f6967821bb03c049ed4
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-06-24 16:31:23 +0545

    GSAP_project_1

--- Project/script.js
+++ Project/script.js
@@ -1,6 +1,38 @@
-gsap.from(".nav h3",{
-    y: -50,
+var animate = gsap.timeline({repeat:-1})
+
+gsap.from(".nav h3", {
+  y: -50,
+  opacity: 0,
+  duration: 1,
+  delay: 0.3,
+  stagger: 0.3,
+});
+
+
+gsap.from("h1", {
     opacity: 0,
     duration: 1,
-    delay: 0.4,
-    stagger: 0.5}) 
+    delay: 1.5,
+})
+
+// gsap.from(".box-body", {
+//     opacity: 0,
+//     duration: 1,
+//     delay: 1.5,
+// })
+
+animate.from(".box1", {
+    opacity: 0,
+    duration: 1,
+    delay: 1,
+})
+
+animate.from(".box2", {
+    opacity: 0,
+    duration: 1,
+})
+
+animate.from(".box3", {
+    opacity: 0,
+    duration: 1,
+})
