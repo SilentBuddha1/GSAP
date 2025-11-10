6c4fff32d6ab1f1c21826d0ed6024e0239a131f5
silentbuddha1 <ramchandrakarki231@gmail.com>
2025-11-10 19:24:54 +0545

    loader improving

--- Project/4/script.js
+++ Project/4/script.js
@@ -1,6 +1,27 @@
-gsap.from(".line h1", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-})
+let tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1, .line h2", {
+  opacity: 0,
+  onStart: function () {
+    let timer = document.querySelector("#line1-part1 h5");
+    let grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        timer.innerHTML = grow++;
+      } else {
+        timer.innerHTML = grow;
+      }
+    }, 33);
+  },
+});
+
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.4,
+  delay: 3.5,
+});
